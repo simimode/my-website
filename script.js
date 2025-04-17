67c8860183aa897a2e4f75659425991aebd6b8f5
Simi Modey <simi.modey@gmail.com>
2025-04-17 13:56:34 -0400

    Add new page

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
+HEAD
 const button = document.getElementById("magicButton");
 const message = document.getElementById("message");
 
 button.addEventListener("click", () => {
   message.textContent = "You clicked the button! Now imagine what else this site can do...";
 });
-=======
-const button = document.getElementById("magicButton");
-const message = document.getElementById("message");
-
-button.addEventListener("click", () => {
-  message.textContent = "You clicked the button! Now imagine what else this site can do...";
-});
->>>>>>> e61c2c10a029a6cfebc4d5ce13337a4be2651e4b

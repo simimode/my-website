c02c8acc5aee59a533ed94c9b4410f84100cac99
Simi Modey <simi.modey@gmail.com>
2025-04-17 14:02:57 -0400

    Edit studypage

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const button = document.getElementById("magicButton");
 const message = document.getElementById("message");
 
 button.addEventListener("click", () => {
-  message.textContent = "You clicked the button! Now imagine what else this site can do...";
+  message.textContent = "congrats";
 });
